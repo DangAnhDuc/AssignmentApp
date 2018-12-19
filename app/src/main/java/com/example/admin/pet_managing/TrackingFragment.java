037d69cffc50fd0110377e04547bbec754587788
AriusAce <trunghao.tran@my.jcu.edu.au>
2018-12-19 19:04:48 +0800

    Adding chat function

---
+++ app/src/main/java/com/example/admin/pet_managing/TrackingFragment.java
@@ -0,0 +1,27 @@
+package com.example.admin.pet_managing;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class TrackingFragment extends Fragment {
+
+    public static TrackingFragment newInstance() {
+        TrackingFragment trackingFragment=new TrackingFragment();
+        return trackingFragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+}
