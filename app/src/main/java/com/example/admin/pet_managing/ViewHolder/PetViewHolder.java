60ed6a04c632a6ff90d2493d49a611110ce7dc5c
AriusAce <trunghao.tran@my.jcu.edu.au>
2018-12-19 18:51:20 +0800

    Adding missing holder file

---
+++ app/src/main/java/com/example/admin/pet_managing/ViewHolder/PetViewHolder.java
@@ -0,0 +1,38 @@
+package com.example.admin.pet_managing.ViewHolder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.admin.pet_managing.Interface.ItemClickListener;
+import com.example.admin.pet_managing.R;
+
+public class PetViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+    public TextView petName;
+    public ImageView petImage;
+
+    private ItemClickListener itemClickListener;
+
+
+
+    public PetViewHolder(View itemView) {
+        super(itemView);
+        petName=(TextView) itemView.findViewById(R.id.petName);
+        petImage=(ImageView) itemView.findViewById(R.id.petImg);
+
+        itemView.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        itemClickListener.onClick(v,getAdapterPosition(),false);
+
+    }
+
+    public void setItemClickListener(ItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
+}
